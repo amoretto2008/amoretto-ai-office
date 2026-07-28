530babf6c1f4120b5c9dd9625730c16de68536d1
amoretto2008 <amoretto2008@gmail.com>
2026-07-28 11:07:18 +0900

    接続診断の新版を読み込む

--- public/standard/app.js
+++ public/standard/app.js
@@ -1,5 +1,5 @@
 (function(){
-  const ASSET_VERSION="18";
+  const ASSET_VERSION="19";
   function load(src){
     return new Promise((resolve,reject)=>{
       const script=document.createElement("script");
