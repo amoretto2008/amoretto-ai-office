aeb3ddf787f2102590026de3feed99d26173431e
amoretto2008 <amoretto2008@gmail.com>
2026-07-29 14:03:40 +0900

    STANDARDの表示整理を読み込む

--- public/standard/app.js
+++ public/standard/app.js
@@ -1,5 +1,5 @@
 (function(){
-  const ASSET_VERSION="28";
+  const ASSET_VERSION="29";
   function load(src){
     return new Promise((resolve,reject)=>{
       const script=document.createElement("script");
@@ -14,6 +14,7 @@
     .then(()=>load("manual-pack.js"))
     .then(()=>load("manual-fixes.js"))
     .then(()=>load("core-content.js"))
+    .then(()=>load("manual-ui-fixes.js"))
     .then(()=>load("course-flow.js"))
     .then(()=>load("staff-ready.js"))
     .then(()=>load("admin-core.js"))
