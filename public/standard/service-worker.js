631b6748fdb42731b91fd468d7c141ec8435e3e5
amoretto2008 <amoretto2008@gmail.com>
2026-07-29 13:44:46 +0900

    STANDARDのPWAキャッシュをマニュアル修正対応へ更新

--- public/standard/service-worker.js
+++ public/standard/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "amoretto-standard-v21";
+const CACHE = "amoretto-standard-v22";
 const STATIC_ASSETS = [
   "/standard/",
   "/standard/index.html",
@@ -7,6 +7,7 @@ const STATIC_ASSETS = [
   "/standard/defaults.js",
   "/standard/core-base.js",
   "/standard/manual-pack.js",
+  "/standard/manual-fixes.js",
   "/standard/core-content.js",
   "/standard/course-flow.js",
   "/standard/staff-ready.js",
